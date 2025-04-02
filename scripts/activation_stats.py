663432f6a4837b8f88a7ea37c742099aea69720e
mrla <malthe@pabst.dk>
2025-04-02 16:19:19 +0200

    More EDA + Activation

---
+++ scripts/activation_stats.py
@@ -0,0 +1,69 @@
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+import os
+
+
+def get_activations(data_folder:str, mac:bool = True):
+    """This function calculates the mean and variance
+       of the ROIs pr. subject and saves them as a CSV file.
+
+    Args:
+        data_folder (str): The folder where the data is located (data/clean/XXXXXXX)
+        mac (bool, optional): If the path is on a mac. Defaults to True.
+
+    Returns:
+        pd.DataFrame: the dataframes one for means and one for variance
+    """
+    path_addon = '.nosync' if mac else ''
+    clean_path = f'../data{path_addon}/clean/{data_folder}'
+    file_list = os.listdir(clean_path)
+    if '.DS_Store' in file_list:
+        file_list.remove('.DS_Store')
+
+    thresholds = []
+
+    for sub in tqdm(file_list): #For each subject
+        #To store the subject ROI means at each timestep 
+        subject_roi_means_time = []
+    
+        all_voxels = []
+
+        subject = np.load(f'{clean_path}/{sub}', allow_pickle = True)
+        #For each ROI 
+        for i in subject.files:
+            roi = subject[i]
+            #Add all voxes to threshold list
+            all_voxels += roi.flatten().tolist()
+            
+            #Make dict of roi means over time
+            roi_mean_time = {'ROI': i}
+
+            #For each timestep get mean
+            for t in range(roi.shape[0]):
+                roi_mean_time[t+1] = float(roi[t].mean())
+            
+            #Add region to subject
+            subject_roi_means_time.append(roi_mean_time)
+
+        all_voxels = np.array(all_voxels)
+
+        thresholds.append({ 'Subject': sub[:-4], 
+                            '10th': np.quantile(all_voxels, 0.10),
+                            '25th': np.quantile(all_voxels, 0.25),
+                            '50th': np.quantile(all_voxels, 0.50),
+                            '75th': np.quantile(all_voxels, 0.75),
+                            '90th': np.quantile(all_voxels, 0.90)})
+
+        #Make dataframe and save
+        data = pd.DataFrame(subject_roi_means_time).T
+        new_header = data.iloc[0] #grab the first row for the header
+        data = data[1:] #take the data less the header row
+        data.columns = new_header #set the header row as the df header
+        data.to_csv(f"../data{path_addon}/stats/{data_folder}/{sub[:-4]}.csv", sep=";")
+    
+    pd.DataFrame(thresholds).to_csv(f"../data{path_addon}/stats/{data_folder}_activation_thresholds.csv", sep=";")
+
+    return None
+
+get_activations(data_folder = 'ADHD200_7')
--- scripts/eda_func.py
+++ scripts/eda_func.py
@@ -1,9 +1,12 @@
-import numpy as np
-import pandas as pd
+from matplotlib import pyplot as plt
 from tqdm import tqdm
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import scipy
+import math
 import os
 import re
-
 
 def exstract_subject_information(data:pd.DataFrame) -> pd.DataFrame:
     """This function exstract information about the subject
@@ -80,3 +83,130 @@ def get_mean_and_var_stats(data_folder:str, mac:bool = True) -> pd.DataFrame:
     var_pr_roi.to_csv(f'data{path_addon}/stats/{data_folder}_variance_subject.csv', sep= ";")
 
     return mean_pr_roi, var_pr_roi
+
+def test_dist(data: pd.DataFrame, regions:list[str]) -> None:
+    """This function test if the values of the ROI are from 
+       the same distribution. It does it for both gender and 
+       the diagnosis
+
+    Args:
+        data (pd.DataFrame): the data which to use
+        regions (list[str]): a list of regions to test
+    """
+    #Gender
+    for roi in regions:
+        a, b = data[data['Gender'] == 'Male'][roi], data[data['Gender'] == 'Female'][roi]
+        p_value = float(scipy.stats.ttest_ind(a, b, 
+                                            equal_var = False, 
+                                            alternative='two-sided').pvalue)
+        print(f'{roi} (Gender): The P-value is {p_value}')
+
+    print('#'*50)
+    #Diagnosis
+    for roi in regions:
+        dia = data['DX'].unique().tolist()
+        sampels = [data[data['DX'] == i][roi] for i in dia]
+        p_value = float(scipy.stats.f_oneway(*sampels).pvalue)
+        print(f'{roi} (Diagnosis): The P-value is {p_value}')
+
+
+def plot_small_multiple_rois(data:pd.DataFrame, regions:list[str], title:str, hue_col:str) -> None:
+    """This fucntion plot small multiple dist plots
+       for each of the regions. Furthermore the hue, 
+       can be 
+
+    Args:
+        data (pd.DataFrame): _description_
+        regions (list[str]): _description_
+        title (str): _description_
+        hue_col (str): _description_
+    """
+    #Get axis limits to uniform x-axis
+    x_min = min(data[regions].min().to_list())-0.1
+    x_max = max(data[regions].max().to_list())+0.1
+
+    #Sub plot size
+    sub_size = math.ceil(math.sqrt(len(regions)))
+
+    #Make subplot
+    fig, axs = plt.subplots(sub_size, sub_size, figsize=(10, 10), tight_layout=True)
+    fig.suptitle(title, fontsize = 20)
+
+    temp_rois = regions.copy()
+
+    #Store y axis
+    y_max = []
+    y_min = []
+
+    for x in range(sub_size):
+        for y in range(sub_size):
+            #If no ROIs left break
+            if len(temp_rois) == 0:
+                break
+            #If top-right add legend and move to the right
+            add_legend = True if [x,y] == [0,sub_size-1] else False
+            
+            #Create subplot
+            sns.kdeplot(data = data, 
+                        x = temp_rois[0], 
+                        hue= hue_col, 
+                        ax = axs[x, y], 
+                        legend= add_legend)
+            
+            #Set title and x-axis range 
+            axs[x, y].set_title(temp_rois[0])
+
+            if add_legend:
+                sns.move_legend(axs[x, y], 
+                                "upper left", 
+                                bbox_to_anchor=(1, 1))
+
+            #Keep track of the y axis -> so it can be uniformed
+            lim = axs[x,y].get_ylim()
+            y_max.append(lim[1])
+            y_min.append(lim[0])
+            temp_rois.remove(temp_rois[0])
+
+    for ax in axs.flat:
+        #Set axis limits and labels
+        ax.set_xlim(x_min, x_max)
+        ax.set_ylim(min(y_min), max(y_max))
+        ax.set(xlabel='Activity', ylabel='Density')
+
+    # Hide x labels and tick labels for top plots and y ticks for right plots
+    for ax in axs.flat:
+        ax.label_outer()
+
+def group_roi_heat_map(data:pd.DataFrame, group:str, title:str, regions:list[str]) -> None:
+    """_summary_
+
+    Args:
+        data (pd.DataFrame): _description_
+        group (str): _description_
+        title (str): _description_
+        regions (list[str]): _description_
+    """
+    #Remove nans from category
+    data = data[~data[group].isna()]
+    #Get unique groups
+    groups = data[group].unique()
+    #Make subplot and add title
+    fig, axs = plt.subplots(len(groups), figsize=(10, 10), tight_layout=True)
+    fig.suptitle(title, fontsize = 20)
+    plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, 
+                        top=1, wspace=0.4, hspace=0.4)
+    #Get min and max for colors
+    x_min = min(data[regions].min())
+    x_max = max(data[regions].max())
+    #Create color map
+    color_map = sns.color_palette("Blues", as_cmap=True)
+    #Plot the heatmaps
+    for x in range(len(groups)):
+        temp_data = data[data[group] == groups[x]]
+        #Create subplot
+        sns.heatmap(temp_data.loc[:, regions],
+                    vmin = x_min,
+                    vmax = x_max,
+                    cmap = color_map, 
+                    ax = axs[x])
+        axs[x].set_title(groups[x])
